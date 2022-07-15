9be4616fb97e34dc8cfeaba82bc818cdee0941da
delaossa <alberto.martinez.de.la.ossa@desy.de>
2022-07-15 13:08:53 +0200

    clean this file

--- libe_opt_postproc/__init__.py
+++ libe_opt_postproc/__init__.py
@@ -1,6 +1,4 @@
-from .__version__ import __version__
-
-from .post_processing import PostProcOptimization
-from .ax_model_manager import AxModelManager
-
-__all__ = ['PostProcOptimization', 'AxModelManager']
+# from .__version__ import __version__
+# from .post_processing import PostProcOptimization
+# from .ax_model_manager import AxModelManager
+# __all__ = ['PostProcOptimization', 'AxModelManager']
